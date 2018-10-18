66ff2a4e6589a02aad742504e4a4696d0532ebdd
Mika88 <mikaspivak@gmail.com>
2018-10-18 17:00:16 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -32,7 +32,6 @@ class Application
   def handle_search(search_term)
     if @@items.include?(search_term)
       return "#{search_term} is one of our items"
-      return "added #{search_term}"
     else
       return "Couldn't find #{search_term}"
     end
